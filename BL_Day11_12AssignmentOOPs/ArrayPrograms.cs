ad273e9abf3a85f0ac39efbf88c43a4cbe00072d
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-05 18:15:53 +0530

    [Shoaib] ArrayPrograms.CountOfDulplicateElements will return number of elements repeating more than once

---
+++ BL_Day11_12AssignmentOOPs/ArrayPrograms.cs
@@ -0,0 +1,37 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace BL_Day11_12AssignmentOOPs
+{
+    internal class ArrayPrograms
+    {
+        public static int CountOfDulplicateElements(int[] arr)
+        {
+            Dictionary<int, int> elementsFrequency = new();
+            foreach (int i in arr.Skip(1))
+            {
+                if (elementsFrequency.ContainsKey(i))
+                {
+                    elementsFrequency[i]++;
+                }
+                else
+                {
+                    elementsFrequency[i] = 1;
+                    //duplicateElements.Add(i, 1);
+                }
+            }
+            int coountOfDuplicates = 0;
+            foreach (int count in elementsFrequency.Values)
+            {
+                if (count > 1)
+                {
+                    coountOfDuplicates++;
+                }
+            }
+            if (coountOfDuplicates <= 0)
+            {
+                return -1;
+            }
+            return coountOfDuplicates;
+        }
+    }
+}
