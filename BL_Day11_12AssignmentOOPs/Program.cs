1005b8a91e65cbcd65c9a4e45bc0e12dcb609c59
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-06 19:40:28 +0530

    [Shoaib] A program to reverse each word of a string at it's own position is introduced

--- BL_Day11_12AssignmentOOPs/Program.cs
+++ BL_Day11_12AssignmentOOPs/Program.cs
@@ -8,7 +8,7 @@
             Console.WriteLine("1.Count total number of duplicate elements in an array"
                 + "\n2.Print all unique elements of array\n3.Frequency of each element" +
                 "\n4.Find Max and min in the array\n5.Inverted right triangle pattern" +
-                "\n6.Rectangular boundary pattern ");
+                "\n6.Rectangular boundary pattern\n7.Revere each word of a string");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -32,6 +32,12 @@
                     break;
                 case 6:
                     PrintingPatterns.RectangularBoundary();
+                    break;
+                case 7:
+                    Console.Write("Enter a string: ");
+                    string input = Console.ReadLine();
+                    string result = StringPrograms.ReverseEachWordAtItsPosition(input);
+                    Console.WriteLine(result);
                     break;
                 default:
                     Console.WriteLine("Invalid Input");
---
+++ BL_Day11_12AssignmentOOPs/StringPrograms.cs
@@ -0,0 +1,35 @@
+﻿namespace BL_Day11_12AssignmentOOPs
+{
+    internal class StringPrograms
+    {
+        internal static string ReverseEachWordAtItsPosition(string input)
+        {
+            char[] delimiters = new char[] { ' ', ',', '.', ';', ':', '!', '?' };
+            string[] words = input.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < words.Length; i++)
+            {
+                words[i] = ReverseString(words[i]);
+            }
+            string result = "";
+            for (int i = 0; i < words.Length; i++)
+            {
+                result += words[i];
+                result += " ";
+            }
+            return result;
+        }
+
+        internal static string ReverseString(string s)
+        {
+            char[] charArray = s.ToCharArray();
+            int len = s.Length - 1;
+            for (int i = 0; i < len; i++, len--)
+            {
+                char temp = charArray[i];
+                charArray[i] = charArray[len];
+                charArray[len] = temp;
+            }
+            return new string(charArray);
+        }
+    }
+}
