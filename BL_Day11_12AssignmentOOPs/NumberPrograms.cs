791654513ef1e4ab74501ef3d84f27603874d5a7
shaikshoaib99 <smdshoaib137@gmail.com>
2023-06-06 23:17:00 +0530

    [Shoaib] A program to calculate sum of digits of a number is introduced

---
+++ BL_Day11_12AssignmentOOPs/NumberPrograms.cs
@@ -0,0 +1,16 @@
+﻿namespace BL_Day11_12AssignmentOOPs
+{
+    internal class NumberPrograms
+    {
+        public static int SumOfDigits(int number)
+        {
+            int sum = 0;
+            while (number != 0)
+            {
+                sum += number % 10;
+                number /= 10;
+            }
+            return sum;
+        }
+    }
+}
